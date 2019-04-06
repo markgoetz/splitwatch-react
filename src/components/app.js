f2fed0087ddcf3dc6404ffb8fb92a9568008c4d7
Mark Goetz <breakawayblue@gmail.com>
2019-04-06 16:24:38 -0500

    CSS updates

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import ControlsView from '../routes/controlsview/ControlsView';
+import IndexView from '../routes/indexview/IndexView';
+import NewSplitView from '../routes/newsplitview/NewSplitView';
+import Header from '../components/Header/Header';
+
 import '../style/index.css';
 
 const initialTimer = {
@@ -13,11 +18,21 @@ export default function(props) {
     const [splits] = useState([]);
 
     return (
-        <div id="app">
-            <ControlsView startTime={timer.startTime}
-                baseTime={timer.baseTime}
-                isPlaying={timer.isPlaying}
-                splits={splits} />
+        <div className="o-app">
+            <div className="o-app__hd">
+                <Header/>
+            </div>
+            <div className="o-app__bd">
+                <Router>
+                    <Route exact path="/controls" render={props => <ControlsView startTime={timer.startTime}
+                    baseTime={timer.baseTime}
+                    isPlaying={timer.isPlaying}
+                    splits={splits} />}></Route>
+                    <Route exact path="/" component={IndexView} />
+                    <Route exact path="/newsplit" component={NewSplitView} />
+                </Router>
+                </div>
+            <div className="o-app__ft"></div>
         </div>
     );
 }
