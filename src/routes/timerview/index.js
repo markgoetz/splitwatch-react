df2f01cb018b2245102b6d5c6de15c34f3374d04
Mark Goetz <breakawayblue@gmail.com>
2019-04-06 16:15:49 -0500

    Remove unneeded React components

--- src/routes/timerview/index.js
+++ src/routes/timerview/index.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import VerticalLayout from '../../components/VerticalLayout/VerticalLayout';
 import TimerComponent from '../../components/TimerComponent/TimerComponent';
 import SplitList from '../../components/SplitList/SplitList';
 import Wrapper from '../../components/Wrapper/Wrapper';
@@ -7,10 +6,10 @@ import Wrapper from '../../components/Wrapper/Wrapper';
 export default class TimerView extends Component {
   render() {
     return (<Wrapper align="left">
-      <VerticalLayout align="left">
+      <div className="o-vertical o-vertical--left">
         <TimerComponent {...this.props} />
         <SplitList splits={this.props.splits} />
-      </VerticalLayout>
+      </div>
     </Wrapper>);
   }
 }
