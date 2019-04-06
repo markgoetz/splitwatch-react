9dcdbada9ae5cbe7daea6161f6dea676aac15011
Mark Goetz <breakawayblue@gmail.com>
2019-04-06 15:51:26 -0500

    Add css for button and form
    Remove Button component

--- src/components/StartSplitButton/StartSplitButton.js
+++ src/components/StartSplitButton/StartSplitButton.js
@@ -1,11 +1,10 @@
 import React, { useContext } from 'react';
 import TimerContext from '../../lib/TimerContext';
-import Button from '../Button/Button';
 
 export default function StartSplitButton(props) {
     const context = useContext(TimerContext);
 
     return context.isPlaying ?
-        <Button isPrimary onClick={() => context.split(Date.now())}>Split</Button> :
-        <Button isPrimary onClick={() => context.start(Date.now())}>Start</Button>;
+        <button className="c-btn c-btn--primary" onClick={() => context.split(Date.now())}>Split</button> :
+        <button className="c-btn c-btn--primary" onClick={() => context.start(Date.now())}>Start</button>;
 }
