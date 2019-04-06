2a142c84e40310a8d4199887b42f5258624fec27
Mark Goetz <breakawayblue@gmail.com>
2019-04-06 16:16:10 -0500

    Remove unneeded references to React components

--- src/routes/controlsview/ControlsView.js
+++ src/routes/controlsview/ControlsView.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-import Header from '../../components/Header/Header';
-import Button from '../../components/Button/Button';
-import VerticalLayout from '../../components/VerticalLayout/VerticalLayout';
-import HorizontalLayout from '../../components/HorizontalLayout/HorizontalLayout';
 import Wrapper from '../../components/Wrapper/Wrapper';
 import TimerProvider from '../../components/TimerProvider/TimerProvider';
 import TimerComponent from '../../components/TimerComponent/TimerComponent';
 import LastSplit from '../../components/LastSplit/LastSplit';
 import SplitList from '../../components/SplitList/SplitList';
 import StartSplitButton from '../../components/StartSplitButton/StartSplitButton';
-import SidebarLayout from '../../components/SidebarLayout/SidebarLayout';
 import TimerContext from '../../lib/TimerContext';
 
 export default class ControlsView extends Component {
@@ -20,30 +15,28 @@ export default class ControlsView extends Component {
     }
 
     render() {
-
         return (<div>
-            <Header />
             <TimerProvider>
                 <Wrapper align="center">
-                    <VerticalLayout align="center">
+                    <div className="o-vertical o-vertical--center">
                         <TimerComponent />
                         <LastSplit />
-                        <SidebarLayout>
-                            <VerticalLayout>
+                        <div className="o-sidebar">
+                            <div className="o-vertical">
                                 <StartSplitButton />
-                                <HorizontalLayout>
+                                <div className="o-horizontal">
                                     <TimerContext.Consumer>
                                         {(context => <>
-                                            <Button onClick={() => context.pause(Date.now())}>Pause</Button>
-                                            <Button onClick={() => context.stop(Date.now())}>Stop</Button>
-                                            <Button onClick={() => context.reset(Date.now())}>Reset</Button>
+                                            <button className="c-btn c-btn--secondary" onClick={() => context.pause(Date.now())}>Pause</button>
+                                            <button className="c-btn c-btn--secondary" onClick={() => context.stop(Date.now())}>Stop</button>
+                                            <button className="c-btn c-btn--secondary" onClick={() => context.reset(Date.now())}>Reset</button>
                                         </> )}
                                     </TimerContext.Consumer>
-                                </HorizontalLayout>
-                            </VerticalLayout>
+                                </div>
+                            </div>
                             <SplitList />
-                        </SidebarLayout>
-                    </VerticalLayout>
+                        </div>
+                    </div>
                 </Wrapper>
             </TimerProvider>
         </div>);
